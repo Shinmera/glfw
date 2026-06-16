a27b7d32de65f35d2ea52318c524a00dc30474b7
Yukari Hafner <shinmera@tymoon.eu>
2026-06-16 11:49:20 +0200

    Start work on adding a glfwGetKeyNameModifiers function

--- src/win32_window.c
+++ src/win32_window.c
@@ -2255,7 +2255,7 @@ void _glfwSetCursorModeWin32(_GLFWwindow* window, int mode)
         updateCursorImage(window);
 }
 
-const char* _glfwGetScancodeNameWin32(int scancode)
+const char* _glfwGetScancodeNameWin32(int scancode, int modifiers)
 {
     if (scancode < 0 || scancode > (KF_EXTENDED | 0xff))
     {
